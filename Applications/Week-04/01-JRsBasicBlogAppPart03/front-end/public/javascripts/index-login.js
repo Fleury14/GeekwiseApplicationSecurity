cf4fb5a52c4ac0b89fca67e71da0222ca0e0aec8
J.R. Ruggiero <fleury14@comcast.net>
2018-02-12 12:04:16 -0800

    imported add post form from old app, doesnt post anything yet

--- Applications/Week-04/01-JRsBasicBlogAppPart03/front-end/public/javascripts/index-login.js
+++ Applications/Week-04/01-JRsBasicBlogAppPart03/front-end/public/javascripts/index-login.js
@@ -1,7 +1,18 @@
+// server address
+let _baseUrl = "http://localhost";
+let _port = "3000";
+
 function loginButtonCheck() {
     console.log('Checking for cookie to see if button needs to be disables...');
     const cookieUserName = MyBlogApp.getCookie('realusername');
     document.querySelector('.login-direct').disabled = cookieUserName ? true : false;
+    document.querySelector('.new-post-button').disabled = !cookieUserName ? true : false;
+}
+
+MyBlogApp.toggleForm = function() {
+    // console.log('toggling form...');
+    const form = document.getElementById('addBlogForm');
+    form.classList.toggle('hide-form');
 }
 
 loginButtonCheck();
